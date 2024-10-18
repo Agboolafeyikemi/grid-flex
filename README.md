387c59511dfd69ec26fe2d9849fa82e4822859f3
user <user@users-MacBook-Pro.local>
2024-10-18 12:24:32 +0100

    genesis commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chapter 10 end: Putting all of Grid together
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/agbo-fey-93/pen/VwobYpO](https://codepen.io/agbo-fey-93/pen/VwobYpO).
+
---
+++ dist/script.js
@@ -0,0 +1,16 @@
+/* Lay out the page using Grid, based on all of the things you've learned so far:
+
+- intrinsic and extrinsic grid
+- number, span, area syntax
+- grid-template-columns and grid-template-rows
+- minmax()
+- subgrid
+- media queries
+
+Media queries will likely happen at 500px, 750px, 875px, 1000px. Use a mobile-first approach.
+
+Basic formatting will not need to be changed.
+
+You may need to modify the HTML.
+
+*/
